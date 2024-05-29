25becfdd254ce482b05116bbc4a3e9ade146f927
Cam <cam@camm.id.au>
2024-05-29 13:45:13 +1000

    fix

--- DailyTimer.cs
+++ DailyTimer.cs
@@ -16,7 +16,7 @@ public static class DailyTimer
 {
     
     [FunctionName("DailyTimer")]
-    public static async Task RunAsync([TimerTrigger("0 3 * * *")] TimerInfo myTimer, ILogger log)
+    public static async Task RunAsync([TimerTrigger("0 0 3 * * *")] TimerInfo myTimer, ILogger log)
     {
         await RunQueries(log);
     }
